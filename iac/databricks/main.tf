ce280b3bfca2319104eef024fa61009be6ab4fab
Koos van Strien <koos.van.strien@sigmadata.nl>
2023-06-16 14:30:55 +0200

    Verwijderen van de user-defined managed identity

--- iac/databricks/main.tf
+++ iac/databricks/main.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   features {}
 }
 
+# Create a databricks workspace with system assigned managed identity
+
 resource "azurerm_databricks_workspace" "example" {
   name                        = var.databricks_workspace_name
   resource_group_name         = var.resource_group_name
@@ -9,9 +11,15 @@ resource "azurerm_databricks_workspace" "example" {
   sku                         = var.databricks_sku
   managed_resource_group_name = var.managed_resource_group_name
 
+  # Set system assigned managed identity
+  identity {
+    type = "SystemAssigned"
+  }
+
   custom_parameters {
     no_public_ip = var.no_public_ip
   }
 
   tags = var.tags
 }
+
